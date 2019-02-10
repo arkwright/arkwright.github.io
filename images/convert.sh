6e21a3328fb95ff963968d4da3ed821c831380e7
arkwright <1445684+arkwright@users.noreply.github.com>
2019-02-10 14:36:55 -0500

    Fix image conversion script

--- images/convert.sh
+++ images/convert.sh
@@ -1,9 +1,12 @@
+# brew install imagemagick
+# brew install potrace
+
 mkdir -p png
 
 for image in $( ls *.png ); do
   filename=$(basename "$image" .png)
 
-  convert $filename.png -colorspace Gray -scale "100%" -auto-gamma -auto-level -brightness-contrast 10x10 $filename.bmp
+  convert $filename.png -type Grayscale -scale "100%" -auto-gamma -auto-level -brightness-contrast 40x40 $filename.bmp
 
   rm $filename.svg 2> /dev/null
   potrace -s -H 400pt -t 10 -z black -C "#444444" --tight $filename.bmp
